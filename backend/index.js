c9014ebf62132e323c190d7756a374e494df46e5
Harry Hibbins <harryhibbins11@gmail.com>
2023-01-30 17:03:05 +0000

    setup mongo on cloud database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const itemsRouter = require("./routes/items");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+// const mongoDB = "mongodb+srv://<username>:<password>@cluster0.ds0ycgm.mongodb.net/<database_name>?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://harry_hibbins:9a8dtUhsmkEGawj@cluster0.ds0ycgm.mongodb.net/Bakery?retryWrites=true&w=majority";
+
+app.use(express.json())
+app.use("/items", itemsRouter);
+
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from Expressingssi!" });
