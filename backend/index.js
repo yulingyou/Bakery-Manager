a4e86540878d028b3dbc32ec77ae1b81320f9627
Michael <dev.mhowells@gmail.com>
2023-01-30 12:52:15 +0000

    added nodemon to backend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from Express!" });
+    res.json({ message: "Hello from Expressingss!" });
   });
 
 app.listen(PORT, () => {
