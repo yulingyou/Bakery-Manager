b66e9b8a71ad3f1baf2af388f749e49ab8341f57
Michael <dev.mhowells@gmail.com>
2023-01-31 14:29:46 +0000

    completed basic testing for initial implementation of add item function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,6 @@
 import './styles.css';
 import React from 'react';
-import { useState } from 'react';
-
-
-function Item({food}) {
-
-  const [counter, setCounter] = useState(0)
-
-  const increaseCount = () => {
-    setCounter((prevCounter) => prevCounter + 1)
-  }
-  
-  const decreaseCount = () => {
-    setCounter((prevCounter) => prevCounter - 1)
-  }
-  
-
-  return (
-    <div class="card w-96 bg-base-100 shadow-xl card-bordered">
-  <figure><img src=""/></figure>
-  <div class="card-body">
-    <h2 class="card-title">{food.itemName}</h2>
-    <p>Price: {food.price}</p>
-    <p>Batch Quantity: {food.batchQuantity}</p>
-    <div class="card-actions justify-end">
-      <button class='btn'onClick={increaseCount}>+</button>
-      <p>{counter}</p>
-      <button class='btn' onClick={decreaseCount}>-</button>
-      <button class="btn btn-primary">Add to basket</button>
-    </div>
-  </div>
-</div>
-  )
-}
+import Item from './Item';
 
 function App() {
   // const [data, setData] = useState(null);
