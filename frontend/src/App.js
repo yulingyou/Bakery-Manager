660de0f237eea4298ded79276246cd160f7c08f4
Michael <dev.mhowells@gmail.com>
2023-01-31 14:44:58 +0000

    changes to organisation after merge with testing branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,7 @@
 import './styles.css';
 import React from 'react';
 import { useState } from 'react';
-
-
-function Item({food}) {
-
-  const [counter, setCounter] = useState(0)
-
-  const increaseCount = () => {
-    setCounter((prevCounter) => prevCounter + 1)
-  }
-  
-  const decreaseCount = () => {
-    setCounter((prevCounter) => prevCounter - 1)
-  }
-  
-
-  return (
-    <div class="m-10 place-content-evenly bg-green card w-96 bg-base-100 shadow-xl card-bordered">
-    <figure><img src="https://www.dinnerbyheston.co.uk/cms/images/_800x418_crop_center-center_82_none/Brown-Bread-Ice-cream-APW-copy.jpg?mtime=1575465674"/></figure>
-      <div class="rounded-b-lg bg-green card-body">
-        <div class="bg-green text-bone">
-          <h1 class="card-title heading">{food.itemName}</h1>
-          <p>Price: {food.price}</p>
-          <p>Batch Quantity: {food.batchQuantity}</p>
-        </div>
-    <div class="card-actions justify-end">
-      <button class='btn btn-circle btn-sm' onClick={increaseCount}>+</button>
-      <p class='text-center'>{counter}</p>
-      <button class='btn btn-circle btn-sm' onClick={decreaseCount}>-</button>
-      <button class="btn gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-        Add to wishlist
-      </button>
-      <button class="btn btn-sm">Add to basket</button>
-      
-    </div>
-  </div>
-  </div>
-  )
-}
+import Item from './Item';
 
 function App() {
   // const [data, setData] = useState(null);
--- frontend/src/Item.js
+++ frontend/src/Item.js
@@ -15,19 +15,26 @@ export default function Item({food}) {
     
   
     return (
-      <div class="card w-96 bg-base-100 shadow-xl card-bordered">
-    <figure><img src=""/></figure>
-    <div class="card-body">
-      <h2 class="card-title">{food.itemName}</h2>
-      <p>Price: {food.price}</p>
-      <p>Batch Quantity: {food.batchQuantity}</p>
+      <div class="m-10 place-content-evenly bg-green card w-96 bg-base-100 shadow-xl card-bordered">
+      <figure><img src="https://www.dinnerbyheston.co.uk/cms/images/_800x418_crop_center-center_82_none/Brown-Bread-Ice-cream-APW-copy.jpg?mtime=1575465674"/></figure>
+        <div class="rounded-b-lg bg-green card-body">
+          <div class="bg-green text-bone">
+            <h1 class="card-title heading">{food.itemName}</h1>
+            <p>Price: {food.price}</p>
+            <p>Batch Quantity: {food.batchQuantity}</p>
+          </div>
       <div class="card-actions justify-end">
-        <button data-cy="increase-btn" class='btn' onClick={increaseCount}>+</button>
-        <p data-cy="counter">{counter}</p>
-        <button data-cy="decrease-btn" class='btn' onClick={decreaseCount}>-</button>
-        <button class="btn btn-primary">Add to basket</button>
+        <button data-cy="increase-btn" class='btn btn-circle btn-sm' onClick={increaseCount}>+</button>
+        <p class='text-center' data-cy="counter">{counter}</p>
+        <button data-cy="decrease-btn" class='btn btn-circle btn-sm' onClick={decreaseCount}>-</button>
+        <button class="btn gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+          Add to wishlist
+        </button>
+        <button class="btn btn-sm">Add to basket</button>
+        
       </div>
     </div>
-  </div>
+    </div>
     )
   }
