04ad35c3ed425142d25be09d306e0264cdbb1b16
Harry Hibbins <harryhibbins11@gmail.com>
2023-01-31 10:50:38 +0000

    Setup env file

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 const express = require("express");
 const itemsRouter = require("./routes/items");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+//Change 'dev' to 'production' when using real database
+process.env.STATUS === 'dev' ?(DATABASE_NAME = process.env.DATABASE)
+: (DATABASE_NAME = process.env.TEST_DATABASE)
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-// const mongoDB = "mongodb+srv://<username>:<password>@cluster0.ds0ycgm.mongodb.net/<database_name>?retryWrites=true&w=majority";
-const mongoDB = "mongodb+srv://harry_hibbins:9a8dtUhsmkEGawj@cluster0.ds0ycgm.mongodb.net/Bakery?retryWrites=true&w=majority";
+const mongoDB = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.ds0ycgm.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`;
 
 app.use(express.json())
 app.use("/items", itemsRouter);
