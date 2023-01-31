afc8d4658317de194a370ddbce94fc8e080d1113
Abigail Minett <106555227+AbbeyJM@users.noreply.github.com>
2023-01-31 14:53:33 +0000

    counter can't go below zero

--- frontend/src/Item.js
+++ frontend/src/Item.js
@@ -9,8 +9,10 @@ export default function Item({food}) {
       setCounter((prevCounter) => prevCounter + 1)
     }
     
-    const decreaseCount = () => {
-      setCounter((prevCounter) => prevCounter - 1)
+  const decreaseCount = () => {
+    if (this.counter > 0) {
+        setCounter((prevCounter) => prevCounter - 1)
+      }
     }
     
   
