6a39571bc1d1ca52a6373989ac95e72abb9634b6
Abigail Minett <106555227+AbbeyJM@users.noreply.github.com>
2023-02-01 10:20:10 +0000

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,25 +15,29 @@ function App() {
   const blueberryMuffin = {
     price: 22,
     itemName: 'Blueberry Muffin',
-    batchQuantity: 12
+    batchQuantity: 12,
+    itemImage: "https://images.squarespace-cdn.com/content/v1/51ceeab3e4b025acba722204/1439679792289-OQW8GOQBUTCYVM7WN29C/Blueberry+Muffins+-+The+Stiers+Aesthetic"
   }
   
   const raspberryMuffin = {
     price: 25,
     itemName: 'Raspberry Muffin',
-    batchQuantity: 12
+    batchQuantity: 12,
+    itemImage: "https://vintagekitchennotes.com/wp-content/uploads/2022/04/Raspberry-muffins-11.jpeg"
   }
   
   const chickenMuffin = {
     price: 2,
     itemName: 'Chicken Muffin',
-    batchQuantity: 22
+    batchQuantity: 22,
+    itemImage: "https://sodelicious.recipes/wp-content/uploads/2017/10/e738d-06.03.2017-r1-var1-chicken-muffin-sandwiches-1200x1200.jpg"
   }
 
   const lemonDrizzleCake = {
     price: 20,
     itemName: 'Lemon Drizzle Cake',
-    batchQuantity: 1
+    batchQuantity: 1,
+    itemImage: "https://charlotteslivelykitchen.com/wp-content/uploads/2021/04/lemon-drizzle-loaf-cake-8.jpg"
   }
   
   const items = [blueberryMuffin, raspberryMuffin, chickenMuffin, lemonDrizzleCake]
--- frontend/src/Item.js
+++ frontend/src/Item.js
@@ -19,20 +19,25 @@ export default function Item({food}) {
 
   const updateBasket = () => {
     if (inBasket){
-      changeText("Add to basket")
+      changeBasketButtonText("Add to basket")
       setInBasket(false)
     }
     else{
-      changeText("In Basket")
+      changeBasketButtonText("In Basket")
       setInBasket(true)
     }
   }
 
-  const changeText = (text) => setBasketText(text);
+  const changeBasketButtonText = (text) => setBasketText(text);
 
     return (
       <div className="m-10 place-content-evenly bg-green card w-96 bg-base-100 shadow-xl card-bordered">
-      <figure><img src="https://www.dinnerbyheston.co.uk/cms/images/_800x418_crop_center-center_82_none/Brown-Bread-Ice-cream-APW-copy.jpg?mtime=1575465674"/></figure>
+        <div className="object-fit contain">
+        <figure class="rounded -lg max-w-lg h-64 relative max-w-sm transition-all duration-300 ">
+            <a href="#">
+              <img class="rounded-lg" src={food.itemImage} />
+          </a>
+          </figure>
         <div className="rounded-b-lg bg-green card-body">
           <div className="bg-green text-bone">
             <h1 className="card-title heading">{food.itemName}</h1>
@@ -43,13 +48,14 @@ export default function Item({food}) {
         <button data-cy="increase-btn" className='btn btn-circle btn-sm' onClick={increaseCount}>+</button>
         <p className='text-center' data-cy="counter">{counter}</p>
         <button data-cy="decrease-btn" class='btn btn-circle btn-sm' onClick={decreaseCount}>-</button>
-        <button className="btn gap-2">
+        {/* <button className="btn gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
           Add to wishlist
-            </button>
+            </button> */}
             <div className="btn" onClick={() => updateBasket()}>{basketText}</div>
       </div>
     </div>
-    </div>
+        </div>
+        </div>
     )
   }
