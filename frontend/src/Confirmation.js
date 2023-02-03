0cadcc4634dbe6ae9b2e1fc85ea936bf4a596140
Natalie Theodoropoulou <natalie.theo@gmail.com>
2023-02-03 12:34:33 +0000

    half the fetch request but i got stuck. wah

--- frontend/src/Confirmation.js
+++ frontend/src/Confirmation.js
@@ -1,9 +1,22 @@
 import './styles.css';
 import React from 'react';
+import {useEffect} from 'react';
 
 const Confirmation = () => {
 return (<div>Your order is confirmed and will be delivered on DATE</div>)
 }
+
+useEffect(() => {
+    fetch('/bakers', {
+        method: 'get'
+    })
+    .then(res => res.json())
+    .then((data) => {
+        console.log(bakers)
+    })
+    
+}
+)
 
 export default Confirmation;
 
