b81b6f679f51e96c9f259e163b93d12766e0a956
Natalie Theodoropoulou <natalie.theo@gmail.com>
2023-02-03 14:17:31 +0000

    one test mostly passing

--- frontend/src/Confirmation.cy.js
+++ frontend/src/Confirmation.cy.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Confirmation from '/confirmation'
+import Confirmation from './confirmation'
 
 describe('<Confirmation />', () => {
         it('displays confirmation message', () => {
             cy.mount(<Confirmation />);
             cy.get('[data-cy="confirmation_message"]').should("have.text", 
             "Your order is confirmed and will be delivered on DATE");
+            
         })}
 )
