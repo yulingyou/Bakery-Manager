573be36062fe953171444e37bcdee28dc437b451
Natalie Theodoropoulou <natalie.theo@gmail.com>
2023-02-06 14:02:46 +0000

    updated page with daisy ui box and home button

--- frontend/src/confirmation.js
+++ frontend/src/confirmation.js
@@ -7,8 +7,10 @@ import {useEffect, useState} from 'react';
 const Confirmation = () => {
 
     const [orderName, setOrderName] = useState(null)
+    // const [orderQuantity, setOrderQuantity] = useState(null)
+    const [orderId, setOrderId] = useState(null)
 
-    
+// fetch request getting information from database of confirmed orders     
 useEffect(() => {
     fetch('/bakers', {
         method: 'get'
@@ -16,18 +18,35 @@ useEffect(() => {
     .then(res => res.json())
     .then((data) => {
         setOrderName(data.confirmedOrder[0].confirmedOrder)
-        console.log(data.confirmedOrder[0].confirmedOrder)
+        // setOrderQuantity(data.confirmedOrder[0]);
+        console.log(data.confirmedOrder[0]);
+        setOrderId(data.confirmedOrder[0].orderId);
     })
-    
-}, [])
+    }) 
 
-
+// confirmation message
 return (
-    <div className='orderConfirmation'>
-        <div className='orderText' data-cy="confirmation_message"><h1>Your order is confirmed and will be delivered on DATE</h1></div>
-        <div className='orderName'> Order: 
-        <p>{orderName} </p></div>
-        <div className='orderQuantity'></div>
+    // <div className='orderConfirmation'>
+    //     <div className='orderText' data-cy="confirmation_message">
+    //         <h1>Your order is confirmed and will be delivered on DATE</h1></div>
+    //     <div className='orderName'> Order: 
+    //     <p>{orderName} </p></div>
+    //     <div className='orderQuantity'></div>
+    // </div>
+    <div className="card w-96 bg-base-100 shadow-xl place-content-center">
+  {/* <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure> */}
+  <div className="card-body">
+  <div className="orderText text-center" data-cy="confirmation_message">
+  <h2><b>Your order is confirmed and will be delivered on DATE.</b></h2></div>
+    <h2 className="orderID text-center">Order ID: {orderId}</h2>
+  
+    <p className="order-name text-center">{orderName}</p>
+
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary">Home</button>
     </div>
-)}
-export default Confirmation;
+  </div>
+</div>
+)
+}
+export default Confirmation; 
