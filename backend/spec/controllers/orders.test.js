984be8f6de0734ee9abd541a1be6c23c14922f51
Elle <lou.d.b.rigodon@gmail.com>
2023-02-06 16:51:50 +0000

    Add updateOrdertest

---
+++ backend/spec/controllers/orders.test.js
@@ -0,0 +1,47 @@
+const app = require("../../index");
+const request = require("supertest");
+const Order = require('../../models/order');
+const Orders = require('../../controllers/orders')
+const User = require('../../models/user');
+const mongoose = require("mongoose");
+
+
+describe("updateOrder", () => {
+  let req, res;
+  beforeEach(() => {
+    req = {
+      user_id: mongoose.Types.ObjectId(),
+      params: {
+        order_id: mongoose.Types.ObjectId()
+      },
+      body: {
+        date_required: ''
+      }
+    };
+    res = {
+      status: jest.fn().mockReturnValue({
+        json: jest.fn()
+      })
+    };
+  });
+
+  it("updates an order", async () => {
+    const user = new User({ _id: req.user_id });
+    await user.save();
+    const order = new Order({
+      _id: req.params.order_id,
+      date_of_order: new Date(),
+      date_required: '02/02/2023'
+    });
+    await order.save();
+
+    await Orders.updateOrder(req, res);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.status().json).toHaveBeenCalledWith({
+      message: "OK",
+      token: expect.any(String)
+    });
+    const updatedOrder = await Order.findById(req.params.order_id);
+    expect(updatedOrder.date_required).toEqual(req.body.date_required);
+  });
+});
