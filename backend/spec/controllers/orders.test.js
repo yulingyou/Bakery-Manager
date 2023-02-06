b346c02f7130805967d3d9b42ee8ae07abc8a964
Harry Hibbins <harryhibbins11@gmail.com>
2023-02-06 15:32:02 +0000

    cant get create order test working, also cant get get basket info test working

--- backend/spec/controllers/orders.test.js
+++ backend/spec/controllers/orders.test.js
@@ -4,7 +4,6 @@ require('dotenv').config({path: './.env.test'});
 
 describe("OrdersController", () => {
   describe('getAll', () => {
-
     it("should return IDs of all orders", async () => {
       const mockResponse = {
         status: jest.fn().mockReturnThis(),
@@ -21,76 +20,73 @@ describe("OrdersController", () => {
     })
   });
   describe("createOrder", () => {
-    it("should add a new ID to a list of all orders", async () => {
+    xit('should add a new ID to a list of all orders', async () => {
+      const mockRequest = { body: { "orders": ['1', '2','3']} };
       const mockResponse = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn()
       };
+  
+      await OrdersController.createOrder(mockRequest, mockResponse);
+      expect(mockResponse.status).toHaveBeenCalledWith(201);
+      expect(mockResponse.json).toHaveBeenCalledWith({ Order: [order] });
+    });
+  });
+  describe('getBatch', () => {
+    it("should return a batch", async () => {
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+
+      const mockOrder = {"_id": "1",  
+      "batch_quantity": 5,
+      "price_per_batch": 32};
+      Order.find = jest.fn((cb) => cb(null, mockOrder));
       
-      const mockOrders = ["1","2"];
-      Order.find = jest.fn((cb) => cb(null, mockOrders));
+      await OrdersController.getAll(null, mockResponse);
       
-      // await OrdersController.createOrder("3", mockResponse);
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({"orders": mockOrder});
+    })
+  });
+  describe("getBasketInfoByID", () => {
+   xit("should return all items in a basket", async () => {
+
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+      const mockRequest = { params: {"id": '10'} };
+      // const mockRequest = {
+      //   params: jest.fn().mockReturnThis()
+      // };
+
+      const mockBasket =  {
+        "_id": "10",
+        "orders": [
+            {
+                "_id": "1",
+                "item": "Blueberry Muffin",
+                "batch_quantity": 19,
+                "price_per_batch": 10,
+              },
+              {
+                "_id": "2",
+                "item": "Raspberry Muffin",
+                "batch_quantity": 5,
+                "price_per_batch": 32,
+            }
+        ],
+      }
+
+      Order.find = jest.fn((cb) => cb(null, mockBasket));
       
-      // expect(mockResponse.status).toHaveBeenCalledWith(201);
-      // expect(mockResponse.json).toHaveBeenCalledWith({"orders": ["1","2","3"]});
+      await OrdersController.getBasketInfoByID(mockRequest, mockResponse);
+      
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({"orders": mockBasket});
+
     })
   })
-  // describe('getAll', () => {
-  //   let mockReq, mockRes, mockNext;
-
-  //   beforeEach(() => {
-  //     mockReq = {
-  //       params: {
-  //         id: "88"
-  //       }
-  //     };
-  //     mockRes = {
-  //       confirmedOrder: null,
-  //       status: jest.fn().mockReturnValue({
-  //         json: jest.fn()
-  //       })
-  //     };
-  //     mockNext = jest.fn();
-  //   });
-
-  //   afterEach(() => {
-  //     jest.resetAllMocks();
-  //   });
-
-  //   it('should return the confirmed order if found', async () => {
-  //     const mockOrder = { confirmedOrder: "Muffin" };
-  //     Baker.findById = jest.fn(() => mockOrder);
-
-  //     await BakersController.getBakerById(mockReq, mockRes, mockNext);
-
-  //     expect(mockRes.baker).toEqual(mockOrder);
-  //     expect(mockNext).toHaveBeenCalled();
-  //     expect(Baker.findById).toHaveBeenCalledWith("88");
-  //   });
-
-  //   it('should return 404 if order is not found', async () => {
-  //     Baker.findById.mockImplementation(() => {
-  //       return null;
-  //     });
-
-  //     await BakersController.getBakerById(mockReq, mockRes, mockNext);
-
-  //     expect(mockRes.status).toHaveBeenCalledWith(404);
-  //     expect(mockRes.status().json).toHaveBeenCalledWith({ message: 'Cannot find order' });
-  //     expect(mockNext).not.toHaveBeenCalled();
-  //   });
-
-  //   it('should return 500 on error', async () => {
-  //     Baker.findById.mockImplementation(() => {
-  //       throw new Error('Error finding order');
-  //     });
-
-  //     await BakersController.getBakerById(mockReq, mockRes, mockNext);
-
-  //     expect(mockRes.status).toHaveBeenCalledWith(500);
-  //     expect(mockRes.status().json).toHaveBeenCalledWith({ message: 'Error finding order' });
-  //   });
-
-  // });
 });
