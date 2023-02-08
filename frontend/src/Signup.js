d33e861189b005d3ef0d4e20dcd8e748c06fcb89
Abigail Minett <106555227+AbbeyJM@users.noreply.github.com>
2023-02-08 12:14:45 +0000

    more styling changes

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -50,34 +50,34 @@ export default function Signup({}) {
 								<div class="relative px-4 py-10 bg-bone border-b-8 border-r-8 border-beige drop-shadow-lg sm:rounded-3xl sm:p-20">
 									<div class="max-w-md mx-auto w-96">
 										<div>
-											<h1 className="mb-12 text-3xl text-center font-heading">Sign up</h1>
+											<h1 className=" text-3xl text-center font-heading">Sign up</h1>
 										</div>
 										<div class="divide-y divide-gray-200">
 										<div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
 											<form onSubmit={handleSubmit}>
 
                                             <div class="relative">
-													<input autoComplete="off" id="companyName" name="companyName" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Company Name" value={ companyName } onChange={(e) => setCompanyName(e.target.value)} />
+													<input autoComplete="off" id="companyName" name="companyName" type="text" class="peer placeholder-transparent h-10 w-full rounded-md text-gray-900 mb-4 focus:outline-none focus:borer-rose-600" placeholder="Company Name" value={ companyName } onChange={(e) => setCompanyName(e.target.value)} />
 													<label for="companyName" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Company Name</label>
 												</div>
                                                 <div class="relative">
-													<input autoComplete="off" id="typeOfBusiness" name="typeOfBusiness" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Type of business" value={ typeOfBusiness } onChange={(e) => setTypeOfBusiness(e.target.value)} />
+													<input autoComplete="off" id="typeOfBusiness" name="typeOfBusiness" type="text" class="peer placeholder-transparent h-10 w-full mb-4 rounded-md text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Type of business" value={ typeOfBusiness } onChange={(e) => setTypeOfBusiness(e.target.value)} />
 													<label for="typeOfBusiness" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Type of business</label>
 												</div>
                                                 <div class="relative">
-													<input autoComplete="off" id="address" name="address" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Type of business" value={ address } onChange={(e) => setAddress(e.target.value)} />
+													<input autoComplete="off" id="address" name="address" type="text" class="peer placeholder-transparent h-10 w-full mb-4 rounded-md text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Type of business" value={ address } onChange={(e) => setAddress(e.target.value)} />
 													<label for="address" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Address</label>
 												</div>
                                                 <div class="relative">
-													<input autoComplete="off" id="phoneNumber" name="phoneNumber" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Type of business" value={ phoneNumber } onChange={(e) => setPhoneNumber(e.target.value)} />
+													<input autoComplete="off" id="phoneNumber" name="phoneNumber" type="text" class="peer placeholder-transparent h-10 w-full mb-4 rounded-md text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Type of business" value={ phoneNumber } onChange={(e) => setPhoneNumber(e.target.value)} />
 													<label for="phoneNumber" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Phone number</label>
 												</div>
 												<div class="relative">
-													<input autoComplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" value={ email } onChange={(e) => setEmail(e.target.value)} />
+													<input autoComplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full mb-4 rounded-md text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" value={ email } onChange={(e) => setEmail(e.target.value)} />
 													<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
 												</div>
 												<div class="relative">
-													<input autoComplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" value={ password } onChange={(e) => setPassword(e.target.value)} />
+													<input autoComplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full rounded-md text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" value={ password } onChange={(e) => setPassword(e.target.value)} />
 													<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
 												</div>
 												<div class="mt-6 relative">
@@ -98,7 +98,7 @@ export default function Signup({}) {
 														active:bg-#A9A9A9 active:shadow-lg
 														transition
 														duration-150
-														ease-in-out"
+														ease-in-out mb-5"
 														onSubmit={handleSubmit} >Submit</button>
 												</div>
 											</form>
@@ -111,10 +111,7 @@ export default function Signup({}) {
 						</div>
 					</div>
 				</div>
-				{/* <div  className="flex justify-start">
-				<img src="https://i.imgur.com/9l1A4OS.jpeg" className="w-48 h-48"></img>
-    		</div> */}
-		</div>
+			</div>
 
 	);
 }
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -61,15 +61,14 @@ export default function LogInForm({}) {
 									<div class="divide-y divide-gray-200">
 										<div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 mr-30">
 											<form onSubmit={handleSubmit}>
-												<div class="relative"> <label> EMAIL
-													<input autoComplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" value={ email } onChange={handleEmailChange} required/>
-													{/* <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label> */}
-													</label>
+												<div class="relative"> 
+													<input autoComplete="off" id="email" name="email" type="text" class="peer placeholder-transparent mt-4 h-10 w-full rounded-md border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" value={ email } onChange={handleEmailChange} required/>
+													<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm mt-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
 												</div>
-												<div class="relative"> <label>PASSWORD
-													<input autoComplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" value={ password } onChange={handlePasswordChange} required/>
-													{/* <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label> */}
-													</label>
+												<div class="relative"> 
+													<input autoComplete="off" id="password" name="password" type="password" class="peer placeholder-transparent mt-4 h-10 w-full rounded-md border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" value={ password } onChange={handlePasswordChange} required/>
+													<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm mt-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+													
 												</div>
 												<div class="mt-6 relative">
 													<button type="submit" value="Submit" className="
@@ -89,11 +88,11 @@ export default function LogInForm({}) {
 														active:bg-#A9A9A9 active:shadow-lg
 														transition
 														duration-150
-														ease-in-out"
+														ease-in-out mb-5"
 													onSubmit={handleSubmit} >Submit</button>
 												</div>
 											</form>
-											<Link to='/signup'><b class='text-blue underline mt-10'>don't have an account yet? Sign up here</b></Link>
+											<Link to='/signup'><b class='text-blue underline'>Don't have an account yet? Sign up here</b></Link>
 										</div>
 										</div>
 									</div>
